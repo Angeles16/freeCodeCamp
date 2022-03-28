77472165447b8899b8281b22014a3c9e9ae96765
Angeles16 <luis_sacbajaa@hotmail.com>
2022-03-27 18:33:50 -0600

    main

---
+++ mi-app/src/components/Testimonios.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../Styles/Testimonio.css'
+
+function Testimonios({imagen, nombre, from, cargo, empresa, texto}) {
+
+	console.log({imagen, nombre, from, cargo, empresa, texto})
+
+	return (
+		<div className="contenido-testimonio">
+			<div className="contenedor-imagen">
+			<img
+				className="imagen-testimonio"
+				src={require(`../imag es/${imagen}.png`)}
+				alt="Foto Emma.png" />
+			</div>
+			<div className="contenedor-texto-testimonio">
+				<div className="nombre-testimonio">{nombre} in {from}</div>
+				<div className="cargo-testimonio">{cargo} at {empresa}</div>
+				<div className="texto-testimonio">"{texto}"</div>
+			</div>
+		</div>
+	)
+}
+
+export default Testimonios;
